f094ca9e3ac5be930b1b3cdf5863b893ab931051
Marco Ceppi <marco@ceppi.net>
2019-11-06 06:55:41 -0500

    :tada: Project Scaffolding

---
+++ README.md
@@ -0,0 +1,1 @@
+# Prophetess Netbox Plugin
---
+++ prophetess/plugins/netbox/__init__.py
@@ -0,0 +1,8 @@
+
+__title__ = 'prophetess-netbox'
+__version__ = '0.0.1'
+__description__ = 'Netbox plugin for Prophetess'
+__author__ = 'Marco Ceppi'
+__author_email__ = 'marco@vapor.io'
+__url__ = 'https://github.com/vapor-ware/prophetess-netbox'
+__license__ = 'GNU General Public License v3.0'
