43b402e30bfa711f9da2a63c6f37663f75b52252
Marco Ceppi <marco@ceppi.net>
2020-03-11 13:19:52 -0400

    :bookmark: 0.3.0

--- prophetess_netbox/__init__.py
+++ prophetess_netbox/__init__.py
@@ -2,7 +2,7 @@
 from .loader import NetboxLoader
 
 __title__ = 'prophetess-netbox'
-__version__ = '0.2.6'
+__version__ = '0.3.0'
 __description__ = 'Netbox plugin for Prophetess'
 __author__ = 'Marco Ceppi'
 __author_email__ = 'marco@vapor.io'
